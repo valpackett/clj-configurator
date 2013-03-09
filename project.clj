43ace604d6da620a658cc269707fcc6c3e12d268
myfreeweb <floatboth@me.com>
2013-03-09 13:56:58 +0400

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next developemnt cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-configurator "0.1.1"
+(defproject clj-configurator "0.1.2-SNAPSHOT"
   :description "A powerful yet simple configuration library"
   :url "https://github.com/myfreeweb/clj-configurator"
   :license {:name "WTFPL"
