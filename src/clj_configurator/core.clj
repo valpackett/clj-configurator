e73120e834a998c0fbb029b295b00eca472c58e7
vijaykiran <mail@vijaykiran.com>
2013-09-19 15:19:22 +0200

    Fix Indentation.

--- src/clj_configurator/core.clj
+++ src/clj_configurator/core.clj
@@ -2,31 +2,31 @@
   (:use [clj-configurator util]))
 
 (deftype Env [] clojure.lang.ILookup
-         (valAt [_ k] (lookup-variants #(System/getenv %) (name k))))
+  (valAt [_ k] (lookup-variants #(System/getenv %) (name k))))
 
 (def env (Env.))
 
 (deftype Props [] clojure.lang.ILookup
-         (valAt [_ k] (lookup-variants #(System/getProperty %) (name k))))
+  (valAt [_ k] (lookup-variants #(System/getProperty %) (name k))))
 
 (def props (Props.))
 
 (defn- process-tree [m sources a]
   (into {}
-        (map (fn [[k v]]
-               (let [path (conj a k)]
-                 (if (map? v)
-                   [k (process-tree v sources path)]
-                   [k (let [r (first (filter (comp not nil?)
-                                             [(fmap #(get-in % path) sources)
-                                              (fmap #(get-in % (map name path)) sources)
-                                              (fmap (interpose-keywords "-" path) sources)
-                                              (fmap (interpose-keywords "." path) sources)
-                                              v]))]
-                        (if (or (nil? v) (instance? (class v) r))
-                          r
-                          (to-type-of v r)))])))
-             m)))
+    (map (fn [[k v]]
+           (let [path (conj a k)]
+             (if (map? v)
+               [k (process-tree v sources path)]
+               [k (let [r (first (filter (comp not nil?)
+                                   [(fmap #(get-in % path) sources)
+                                    (fmap #(get-in % (map name path)) sources)
+                                    (fmap (interpose-keywords "-" path) sources)
+                                    (fmap (interpose-keywords "." path) sources)
+                                    v]))]
+                    (if (or (nil? v) (instance? (class v) r))
+                      r
+                      (to-type-of v r)))])))
+         m)))
 
 (defn config [& {:keys [defaults sources]
                  :or {sources [props env]}}]
